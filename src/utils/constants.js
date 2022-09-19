0e8d172aac300c9f736f50750cacf1f7ef12db7b
coolpythoncodes <coolpythoncodes@gmail.com>
2022-09-19 14:28:36 +0100

    changed contract address

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 import RadenuContractAbi from "src/utils/Radenu.json"
 import RadenuTokenContractAbi from "src/utils/Token.json"
 
-const RadenuContractAddress = "0x2EE671e270Ba027e7Dd0a943373bdD83F31aEE0d"
+const RadenuContractAddress = "0x40fb4204dDe488f34b9d9E0056d0FE8f6ab38585"
 const RadenuTokenContractAddress = "0x55d37DB5Ce7b2bE7f05DA00fF30A9e1c8f5D8437"
 const exchangeRate = 700
 const orderState = ['INITIATED', 'ACCEPTED', 'COMPLETED', 'FUFILLED', 'CANCELLED', 'INDISPUTE']
