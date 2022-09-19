777f4c84a2d43e5a6e8282035c70ebef43d35491
coolpythoncodes <coolpythoncodes@gmail.com>
2022-09-19 21:15:54 +0100

    minor fixes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ import RadenuContractAbi from "src/utils/Radenu.json"
 import RadenuTokenContractAbi from "src/utils/Token.json"
 
 const RadenuContractAddress = "0x40fb4204dDe488f34b9d9E0056d0FE8f6ab38585"
-const RadenuTokenContractAddress = "0x55d37DB5Ce7b2bE7f05DA00fF30A9e1c8f5D8437"
+const RadenuTokenContractAddress = "0x5aCb077D92c3F87fd56ED0D14d24ffcec3298C4d"
 const exchangeRate = 700
 const orderState = ['INITIATED', 'ACCEPTED', 'COMPLETED', 'FUFILLED', 'CANCELLED', 'INDISPUTE']
 
